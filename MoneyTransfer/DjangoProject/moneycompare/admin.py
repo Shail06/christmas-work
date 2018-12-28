eac9ddb5d47828a21f755c0eb2d5ca9fd8392501
shail06 <shailesh.wadhwa@gmail.com>
2018-12-28 14:22:44 +0000

    removed old project

--- MoneyTransfer/DjangoProject/moneycompare/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Vendor, Endpoint, QuoteParameter
-
-admin.site.register(Vendor)
-admin.site.register(Endpoint)
-admin.site.register(QuoteParameter)
--- MoneyTransfer/DjangoProject/moneycompare/urls.py
+++
@@ -1,1 +0,0 @@
-from django.urls import pathfrom . import viewsurlpatterns = [path('', views.index, name = 'index'),]
